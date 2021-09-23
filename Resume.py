3f42debe9c770cdb0fb8bcbd8ed0c014ef067f8f
jrallred125 <39539774+jrallred125@users.noreply.github.com>
2021-09-23 16:43:05 -0600

    Add files via upload
    Updated files and wrote a python script to greek a json file.

---
+++ Resume.py
@@ -0,0 +1,71 @@
+import json
+import random
+import sys
+
+def convert_letter(cha):
+    vowel = ['a','e','i','o','u']
+    constant = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    
+    if cha.lower() in vowel:
+        if cha.islower():
+            return random.choice(vowel)
+        else:
+            return random.choice(vowel).upper()
+    elif cha.isalpha():
+        if cha.islower():
+            return random.choice(constant)
+        else:
+            return random.choice(constant).upper()
+    else:
+        return cha
+    
+def build_string(string):
+    if string.startswith('https'):
+        return string
+    return_string =''
+    for char in string:
+        return_string += convert_letter(char)
+    return return_string
+
+def build_list(cur_list):
+    new_list = []
+    for obj in cur_list:
+        if isinstance(obj,str):
+            new_list.append(build_string(obj))
+        if isinstance(obj, list):
+            new_list.append(build_list(obj))
+        if isinstance(obj, dict):
+            new_list.append(build_dict(obj))
+    return new_list
+
+def build_dict(cur_dict):
+    new_dict = {}
+    for key in cur_dict:
+        if isinstance(cur_dict[key],str):
+            new_dict[key] = build_string(cur_dict[key])
+        if isinstance(cur_dict[key], list):
+            new_dict[key] = build_list(cur_dict[key])
+        if isinstance(cur_dict[key], dict):
+            new_dict[key] = build_dict(cur_dict[key])
+    return new_dict
+
+def parse_json(file):
+    if isinstance(file,list):
+        return build_list(file)
+    if isinstance(file, dict):
+        return build_dict(file)
+    if isinstance(file,str):
+        return build_string(file)
+
+
+
+def main(argv):
+    with open(argv[1], 'r', encoding='utf-8') as infile:
+        json_obj = json.load(infile)
+
+
+    with open(f'greeked{argv[1]}', 'w', encoding='utf-8') as outfile:
+        json.dump(parse_json(json_obj), outfile, ensure_ascii=False, indent=4)
+
+if __name__ == "__main__":
+    main(sys.argv)
--- index.js
+++ index.js
@@ -39,45 +39,45 @@ $(document).on('click','#easterEgg',function(){
 
 function displayInfo(info){
     // Step 1. Make a string to hold the information
-    var infoStr = '<h1 title="This is who I am">'; 
+    var infoStr = `<h1 title="This is who I am">`; 
     var nameStart = info.name.slice(0, info.name.length-1);
     var nameEnd = info.name.slice(info.name.length - 1, info.name.length); 
-    infoStr += nameStart + '<span id="easterEgg">' + nameEnd +'</span></h1><p>' + info.phone + '<br>' + info.email + '<br> <a href="'+info.git +'">Git</a> </p>';
+    infoStr += `${nameStart}<span id="easterEgg">${nameEnd}</span></h1><p>${info.phone}<br>${info.email}<br><a href="${info.git}">Git</a> </p>`;
     // Step 2. Update the DOM 
     $('#information').html(infoStr);
 }
 
 function displayObjective(obj){
     // Update the DOM with the objective
-    $('#objective').html('<p>' + obj + '</p>');
+    $('#objective').html(`<p>${obj}</p>`);
 }
 
 function displayEducation(ed){
     // Step 1. Make a string to hold the education information
-    var edStr = '<p><b>' + ed.degree + '</b>, ' + ed.date + '<br>' + ed.schoolName + ', ' + ed.location + '<p><b>Projects</b></p>';
+    var edStr = `<p><b>${ed.degree}</b>, ${ed.date}<br>${ed.schoolName}, ${ed.location}<p><b>Projects</b></p>`;
     // Step 2. Make a string to hold the course list
-    var projectsList = '<ul>';
+    var projectsList = `<ul>`;
     // Step 3. Use a for of loop to get the courses
     for(var project of ed.projects){
-        projectsList += '<li>' + project + '</li>';
+        projectsList += `<li>${project}</li>`;
     }
-    projectsList += '</ul>';
+    projectsList += `</ul>`;
     // Step 4. Updat the DOM with the course list
-    $('#education').html(edStr + projectsList);
+    $('#education').html(`${edStr}${projectsList}`);
 
 }
 
 function displayExperience(experience){ 
     // Step 1. Make a sting to hold the job information
-    var jobsList ='';
+    var jobsList =``;
     // Step 2. Use a for of loop to get the information
     for(var work of experience){
-        jobsList += '<p><b>' + work.title + ', ' + work.dates + '</b><br>' + work.companyName + ', ' + work.location + '<ul>';
+        jobsList += `<p><b>${work.title}, ${work.dates}</b><br>${work.companyName}, ${work.location}<ul>`;
         // Step 3. Use a for of loop to get the duties information
         for(var duty of work.duties){
-            jobsList += '<li>' + duty + '</li>';
+            jobsList += `<li>${duty}</li>`;
         }
-        jobsList += '</ul>';
+        jobsList += `</ul>`;
     }
 
     // Step 4. Update the DOM
@@ -86,12 +86,12 @@ function displayExperience(experience){
 
 function displaySkills(skills){
     // Step 1. Make a string to hold the skill information
-    var skillsList = '<ul>';
+    var skillsList = `<ul>`;
     // Step 2. Use a for of loop to get the information
     for(var skill of skills){
-        skillsList += '<li>' + skill + '</li>';
+        skillsList += `<li>${skill}</li>`;
     }
-    skillsList += '</ul>';
+    skillsList += `</ul>`;
     // Step 3. Update the DOM
     $('#skills').html(skillsList); 
 }
